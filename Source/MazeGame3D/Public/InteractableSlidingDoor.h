fee2d6356c56a3272c8bf4b17160df09aeee3d59
ANelsonABK <Alexanderia.Nelson@activision.com>
2022-08-23 16:21:59 -0400

    Added Sliding Door Component

---
+++ Source/MazeGame3D/Public/InteractableSlidingDoor.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "InteractableSlidingDoor.generated.h"
+
+
+class UDoorInteractionComponent;
+
+UCLASS()
+class MAZEGAME3D_API AInteractableSlidingDoor : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+
+//public:
+	//AInteractableSlidingDoor();
+
+//protected:
+//	UPROPERTY(EditAnywhere, NoClear)
+//		UDoorInteractionComponent* DoorInteractionComponent;
+};
