2d254edc2af6aa44afb013a76db3fce59ce622f0
ANelsonABK <Alexanderia.Nelson@activision.com>
2022-08-23 12:37:33 -0400

    Added Player Controller class and Player Animation Blueprint

---
+++ Source/MazeGame3D/Public/MazeGamePlayerController.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MazeGamePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAZEGAME3D_API AMazeGamePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+
+public:
+	AMazeGamePlayerController() {}
+
+};
