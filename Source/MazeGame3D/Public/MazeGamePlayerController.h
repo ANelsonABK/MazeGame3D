ae4c9467e8526f9dff278a602ec475221e6f2d42
ANelsonABK <Alexanderia.Nelson@activision.com>
2022-08-25 14:10:35 -0400

    Another attempt to organize

--- Source/MazeGame3D/Public/MazeGamePlayerController.h
+++ Source/MazeGame3D/Public/MazeGamePlayerController.h
@@ -18,4 +18,31 @@ class MAZEGAME3D_API AMazeGamePlayerController : public APlayerController
 public:
 	AMazeGamePlayerController() {}
 
+
+protected:
+	void SetupInputComponent() override;
+
+	void RequestMoveForward(float AxisValue);
+	void RequestMoveRight(float AxisValue);
+	void RequestLookUp(float AxisValue);
+	void RequestLookRight(float AxisValue);
+	void RequestJump();
+	void RequestStopJump();
+
+	void RequestCrouchStart();
+	void RequestCrouchEnd();
+
+	void RequestSprintStart();
+	void RequestSprintEnd();
+
+	/* Base lookup rate, in deg/sec. Other scaling may affect final lookup rate. */
+	UPROPERTY(EditAnywhere, Category = "Look")
+		float BaseLookupRate = 90.0f;
+
+	/* Base lookright rate, in deg/sec. Other scaling may affect final lookup rate. */
+	UPROPERTY(EditAnywhere, Category = "Look")
+		float BaseLookRightRate = 90.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+		float SprintSpeed = 1200.0f;
 };
